4fb0bdaaf598e94dfcf13abbfbb2bf9273b7d7ca
olegmaslov <oleg.gnome@yandex.ru>
2022-05-18 20:31:29 +0300

    lesson 4

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,10 @@
+import json
+
+from django.conf import settings
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
-from datetime import datetime
+
+from mainapp.models import News
 
 
 class ContactsView(TemplateView):
@@ -52,31 +57,12 @@ class NewsView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
-        context_data['object_list'] = [
-            {
-                'title': 'Новость раз',
-                'preview': 'Превью к новости раз',
-                'date': datetime.now()
-            }, {
-                'title': 'Новость два',
-                'preview': 'Превью к новости два',
-                'date': datetime.now()
-            }, {
-                'title': 'Новость три',
-                'preview': 'Превью к новости три',
-                'date': datetime.now()
-            }, {
-                'title': 'Новость четыре',
-                'preview': 'Превью к новости четыре',
-                'date': datetime.now()
-            }, {
-                'title': 'Новость пять',
-                'preview': 'Превью к новости пять',
-                'date': datetime.now()
-            }, {
-                'title': 'Новость шесть',
-                'preview': 'Превью к новости шесть',
-                'date': datetime.now()
-            }
-        ]
+        context_data['object_list'] = News.objects.all()
         return context_data
+
+    def get(self, *args, **kwargs):
+        query = self.request.GET.get('q', None)
+        if query:
+            return HttpResponseRedirect(f'https://google.ru/search?q={query}')
+
+        return super().get(*args, **kwargs)
